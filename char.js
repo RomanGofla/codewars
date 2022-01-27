c5da706b607744d83783cf1c907c6742d319cb27
Roman Gofla <gofla95@list.ru>
2022-01-27 10:51:16 +0300

    Exercises

---
+++ char.js
@@ -0,0 +1,15 @@
+function solution(str) {
+  if (str.length == 0) {
+    return []
+  }
+  let double = str.match(/.{1,2}/g)
+  let splited = double.toString().split(',')
+  let lastElem = splited[splited.length - 1]
+  if (lastElem.length == 1) {
+    splited.pop(-1)
+    splited.push(lastElem.toString() + '_')
+  }
+  return splited
+}
+
+console.log(solution(''));
