6899b1aec106e39b55beb635c9cc981a230f6def
Roman Gofla <gofla95@list.ru>
2022-02-01 09:29:48 +0300

    Done

---
+++ domain.js
@@ -0,0 +1,11 @@
+function domainName(url){
+    url = url.replace('http://', '')
+    url = url.replace('https://', '')
+    url = url.replace('www.', '')
+    
+    return url.split('.')[0]
+}
+
+
+const url = "http://github.com/carbonfive/raygun"
+console.log(domainName(url))
